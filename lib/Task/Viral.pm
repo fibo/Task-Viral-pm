01a428eb41db65ed693e2a66e7a1b39a682c136d
fibo <casati_gianluca@yahoo.it>
2013-05-07 15:08:44 +0100

    dzilified

---
+++ lib/Task/Viral.pm
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+package Task::Viral;
+# ABSTRACT: Conquering hosts
+
+1;
+
+=head1 DESCRIPTION
+
+This bundle includes all the stuff you need to build a chain of hosts with Perl installed.
+
+=head1 STUFF INCLUDED
+
+=item Task::Pinto
+
+=item App::cpanminus 
+
+=item App::perlbrew
+
+=cut
+
