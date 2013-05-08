f1e6d8d4c76b3d0e98bfb460a69895d7df7e06ad
fibo <casati_gianluca@yahoo.it>
2013-05-08 10:46:51 +0200

    need more explanation

--- lib/Task/Viral.pm
+++ lib/Task/Viral.pm
@@ -9,6 +9,11 @@ package Task::Viral;
 
 This bundle includes all the stuff you need to build a chain of hosts with Perl installed.
 
+Suppose you have the following topology.
+
+    Internet <- host1 <--> host2
+
+
 =head1 STUFF INCLUDED
 
 =item Task::Pinto
